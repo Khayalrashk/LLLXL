258d0b1d6fa638cbceb236fd1f080655754675f8
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-29 19:13:07 +0300

    Update السورس.py

--- ZeMusic/plugins/play/السورس.py
+++ ZeMusic/plugins/play/السورس.py
@@ -10,31 +10,32 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from strings.filters import command
 from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from ZeMusic import app
+import config
 from random import  choice, randint
 
                 
 @app.on_message(
-    command(["سورس","‹ السورس ›","زد إي","السورس", "سورس زد إي"])
+    command(["سورس","‹ السورس ›","سورس لارين","السورس", "لارين ميوزك"])
 )
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/41a777f089288f7ad2571.jpg",
         caption=f"""**ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ . .
- [🔱 𝐒𝐎𝐔𝐑𝐂𝐄 𝐙𝐄 🔱](https://t.me/Source_Ze)**""",
+ [• 𝐒𝐨𝐮𝐫𝐜𝐞 𝐋𝐚𝐫𝐢𝐧 ♩](https://t.me/SourceLarin)**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "المطور", url=f"https://t.me/ELHYBA"), 
+                        text="𝐃𝐞𝐯🎖", user_id=config.OWNER_ID), 
                     
                 
                     InlineKeyboardButton(
-                        "‹ الدعم ›", url=f"https://t.me/ZeSupport"),
+                        text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)),
                 ],[
                     
                 
                     InlineKeyboardButton(
-                        "‹ السورس ›", url=f"https://t.me/Source_Ze"),
+                        text=config.LARIN_NAME, url=config.LARIN_LINK)),
                 
         ],
 
