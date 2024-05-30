78b7dc2bc8afc0e5d577a18e94200787017c956b
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-30 03:09:36 +0300

    Update id.py

--- ZeMusic/plugins/play/id.py
+++ ZeMusic/plugins/play/id.py
@@ -1,113 +1,125 @@
 import asyncio
-import pyrogram
 from pyrogram import Client, filters
+from ZeMusic import app
+import random
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from typing import Union
-from pyrogram.types import InlineKeyboardButton
-from ZeMusic import app
-from ZeMusic.utils.database import is_on_off
-from ZeMusic import app
-import re
-import sys
-import os
-import random
-import config
-from time import time
-from os import getenv
-from dotenv import load_dotenv
-from pyrogram import filters
+
+
+
+iddof = []
+@app.on_message(
+    command(["قفل الايدي","تعطيل الايدي"])
+    & filters.group
+    & ~filters.edited
+)
+async def iddlock(client, message):
+   get = await app.get_chat_member(message.chat.id, message.from_user.id)
+   if get.status in ["creator", "administrator"]:
+      if message.chat.id in iddof:
+        return await message.reply_text("تم معطل من قبل🔒")
+      iddof.append(message.chat.id)
+      return await message.reply_text("تم تعطيل الايدي بنجاح ✅🔒")
+   else:
+      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+
+@app.on_message(
+    command(["فتح الايدي","تفعيل الايدي"])
+    & filters.group
+    & ~filters.edited
+)
+async def iddopen(client, message):
+   get = await app.get_chat_member(message.chat.id, message.from_user.id)
+   if get.status in ["creator", "administrator"]:
+      if not message.chat.id in iddof:
+        return await message.reply_text("الايدي مفعل من قبل ✅")
+      iddof.remove(message.chat.id)
+      return await message.reply_text("تم فتح الايدي بنجاح ✅🔓")
+   else:
+      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
 
 
 
 
-load_dotenv()
-
-
-
-
-def get_file_id(msg: Message):
-
-    if msg.media:
-
-        for message_type in (
-
-            "photo",
-
-            "animation",
-
-            "audio",
-
-            "document",
-
-            "video",
-
-            "video_note",
-
-            "voice",
-
-            "contact",
-
-            "dice",
-
-            "poll",
-
-            "location",
-
-            "venue",
-
-            "sticker",
-
-        ):
-
-            obj = getattr(msg, message_type)
-
-            if obj:
-
-                setattr(obj, "message_type", message_type)
-
-                return obj
-
-
-@app.on_message(filters.regex("ا", "ايدي") & filters.group)
-async def khalid(client: Client, message: Message):
-
+@app.on_message(
+    command(["ايدي","id","ا"])
+    & filters.group
+    & ~filters.edited
+)
+async def iddd(client, message):
+    if message.chat.id in iddof:
+      return
     usr = await client.get_chat(message.from_user.id)
-
     name = usr.first_name
-
-    bio = usr.bio
-
-
-
-
-    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
-
-                    await message.reply_photo(photo.file_id,       caption=f"""**↯ : وفي النهاية أنتم السيئون وهم الأبرياء**
-            
-**↯ : اسمك : › {message.from_user.mention}**
-                    
-**↯ : معرفك : › @{message.from_user.username}**
-                    
-**↯ : ايديك : › `{message.from_user.id}`**
-                    
-**↯ : البايو : › \n {bio}**""", 
-
-        reply_markup=InlineKeyboardMarkup(
-
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"""🤡 ¦𝙽𝙰𝙼𝙴 :{message.from_user.mention}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{message.from_user.username}\n🎃 ¦𝙸𝙳 :`{message.from_user.id}`\n💌 ¦𝙱𝙸𝙾 :{usr.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙶𝚁𝙾𝚄𝙿 :`{message.chat.id}`""", 
+    reply_markup=InlineKeyboardMarkup(
             [
-
                 [
-
                     InlineKeyboardButton(
-                        text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)
+                        name, url=f"https://t.me/{message.from_user.username}")
                 ],
-
             ]
-
         ),
-
     )
-    
-    
+
+
+
+iddof = []
+@app.on_message(
+    command(["قفل جمالي","تعطيل جمالي"])
+    & filters.group
+    & ~filters.edited
+)
+async def lllock(client, message):
+   get = await app.get_chat_member(message.chat.id, message.from_user.id)
+   if get.status in ["creator", "administrator"]:
+      if message.chat.id in iddof:
+        return await message.reply_text("جمالي معطل من قبل✅")
+      iddof.append(message.chat.id)
+      return await message.reply_text(" تم تعطيل جمالي بنجاح✅🔒")
+   else:
+      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+
+@app.on_message(
+    command(["فتح جمالي","تفعيل جمالي"])
+    & filters.group
+    & ~filters.edited
+)
+async def idljjopen(client, message):
+   get = await app.get_chat_member(message.chat.id, message.from_user.id)
+   if get.status in ["creator", "administrator"]:
+      if not message.chat.id in iddof:
+        return await message.reply_text("جمالي مفعل من قبل✅")
+      iddof.remove(message.chat.id)
+      return await message.reply_text("تم فتح جمالي بنجاح ✅🔓")
+   else:
+      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+
+
+
+
+@app.on_message(
+    command(["جمالي"])
+    & filters.group
+    & ~filters.edited
+)
+async def idjjdd(client, message):
+    if message.chat.id in iddof:
+      return
+    usr = await client.get_chat(message.from_user.id)
+    name = usr.first_name
+    i = ["0","10", "15","20", "25","30","35", "40","45", "50","55", "60"," 66", "70","77", "80","85", "90","99", "100","1000" ]
+    ik = random.choice(i)
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"نسبه جمالك يا مز انت \n│ \n└ʙʏ: {ik} %😂❤️", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        ),
+    )
+       
